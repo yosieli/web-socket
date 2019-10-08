597bc545afb8e8bb67918111068585aeacb4bc80
feven tsegay <feventsegay6@gmail.com>
2019-10-08 16:42:36 -0500

    message while typing done

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express')
-var socket = require('socket.io')
+var express = require('express');
+var socket = require('socket.io');
 
 // App setup
+var app = express();
+var server = app.listen(3000, function(){
+    console.log('listening for requests on port 3000,');
+});
 
-var app = express()
-var server = app.listen(3000, function (){
-    console.log('listening on port 3000')
-})
+// Static files
+app.use(express.static('public'));
 
-//static files
-app.use(express.static('public'))
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
 
-//npm install socket.io --save
+    console.log('made socket connection', socket.id);
 
-//socket setup
-var io = socket(server)
- io.on('connection', function(socket){
-     console.log('made socket connection',socket.id)
- })
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
